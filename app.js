c1416ad43e02d49c53aa655eccf200f1087540b9
herbertamakloe <56282004+herbertamakloe@users.noreply.github.com>
2020-04-09 00:54:39 +0800

    Add cv files

---
+++ app.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing", {
+    strings: [
+        "Welcome",
+        "Akwaaba",
+        "欢迎",
+        "Accueil",
+        "Bienvenido",
+        "Willkommen",
+        // "Arabic"
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+    showCursor: false
+});
---
+++ default.js
@@ -0,0 +1,32 @@
+// document.querySelector('.viewplate').addEventListener('click', function(){
+//     document.querySelector('.sameplates').style.height = '300px';
+//     document.querySelector('.viewplate').style.display = 'none';
+//     // document.querySelector('.backwords-p').style.display = 'none';
+//     document.querySelector('.backwords-p').textContent = 'This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! This is Herbert trying to see if his javascript code works. This is his first time using it and he is so excited! ';
+// });
+
+// document.querySelector('#plateback').addEventListener('mouseleave', function(){
+//     document.querySelector('.sameplates').style.transform = 'rotateX(360deg)';
+// });
+
+
+// var typed = new Typed('.occupation', {
+//     strings: ["First sentence.", "Second sentence."],
+//     typeSpeed: 0
+//   });
+
+var typed = new Typed(".typing", {
+    strings: [
+        "Leader", 
+        "Friend",
+        "Web Designer",
+        "Motivator",
+        "Trillionaire",
+        "Freelancer",
+        "Philanthropist"
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+    showCursor: false
+  });
